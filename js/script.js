b2d881f528410e557bd681f8f55ab26ea415f129
Matheus Garcia <matheusgarciajbs@gmail.com>
2023-04-12 12:39:35 -0300

    modificado logicas no javascript e media query

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const chao = document.querySelector('.chao');
+const reset = document.getElementById('reiniciar');
+const score = document.getElementById('score');
+const animacaoScore = document.querySelector('#score');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -12,32 +15,50 @@ const jump = () => {
     }, 500);
 }
 
-const loop = setInterval(() => {
+let loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-    const chaoPosition = chao.backgroundPosition;
+    
+    const chaoPosition = window.getComputedStyle(chao).backgroundPosition.replace('px', '');
+    const chaoPositionSemPX = chaoPosition.replace('%', '');
+    const chaoNum = parseFloat(chaoPositionSemPX);
+    
 
-    console.log(chaoPosition);
-
-    if(pipePosition <=120 && pipePosition > 0 && marioPosition< 170) {
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`
-        
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
-        
-        
-        mario.src = './img/game-over.png';
-        mario.style.width = '75px';
-        mario.style.marginLeft = '45px';
-
-        chao.style.animation = 'none';
-        chao.style.backgroundPosition = `${chaoPosition}`;
-
-        clearInterval(loop);
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition< 170) {
+        gameOver(pipePosition, marioPosition, chaoNum);
     }
 
 }, 10);
 
+const gameOver = (pipePosition, marioPosition, chaoNum) => {
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`
+    
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+    
+    mario.src = './img/game-over.png';
+    mario.style.width = '75px';
+    mario.style.marginLeft = '45px';
+
+    chao.style.animation = 'none';
+    chao.style.backgroundPosition = `${chaoNum}`;
+    animacaoScore.style.animation = 'score 1s infinite';
+    
+    reset.style.display = "inline";
+    clearInterval(loop);
+    clearInterval(loopPontos);
+}
+
+
+let pontos = 0;
+let loopPontos = setInterval(() => {
+    pontos++;
+    score.innerText = `Score: ${pontos}`
+},100);
+
 document.addEventListener('keydown', jump);
+document.getElementById('reiniciar').addEventListener('click', () => {
+    location.reload();
+})
